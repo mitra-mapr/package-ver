b3776234f1227166e44a3ef916241cb4e0e86bb2
Brandon Lee <blee@maprtech.com>
2016-04-07 11:21:00 -0700

    More dynamic parsing of repo meta data, since repos on Artifactory differ from ones manually created

--- scripts/getPkgListFromURL.py
+++ scripts/getPkgListFromURL.py
@@ -9,7 +9,7 @@ import re
 import argparse
 
 centospatt = "redhat|centos|rpm"
-ubuntupatt = "ubuntu|deb|apt"
+ubuntupatt = "ubuntu|deb|apt|trusty"
 
 
 def setup():
@@ -23,14 +23,15 @@ def setup():
 
 def openPage(url, username, password):
     request = urllib2.Request(url)
-    base64String = base64.standard_b64encode('%s:%s' % (username, password))
-    request.add_header("Authorization", "Basic %s" % base64String)
+    if username:
+        base64String = base64.standard_b64encode('%s:%s' % (username, password))
+        request.add_header("Authorization", "Basic %s" % base64String)
     page = urllib2.urlopen(request)
     return page
 
 
 def find_centos_rpms(args):
-    url = args.URL   
+    url = args.URL
     page = openPage(url, args.user, args.pw)
     soup = BeautifulSoup(page, 'html.parser')
     soup.prettify()
@@ -38,11 +39,17 @@ def find_centos_rpms(args):
     ecopatt = "([45]\.x)"
     ecomatch = re.search(ecopatt, url)
 
+    ecoVersion = ''
+    try:
+        ecoVersion = '%s' % ecomatch.group(0)
+    except:
+        ecoVersion = '5.x'
+
     pattern = "(?P<package_name>[a-zA-Z\-]+\d?)-(?P<version>\d[\.\d\-]+\d)"
     for anchor in soup.findAll('a', href=True, string=re.compile("rpm", re.IGNORECASE)):
         # print anchor['href']
         match = re.search(pattern, anchor['href'])
-        print "%s:%s:%s:%s" % (match.group('package_name'), match.group('version'), ecomatch.group(0), "centos")
+        print "%s:%s:%s:%s" % (match.group('package_name'), match.group('version'), ecoVersion, "centos")
 
 
 def find_ubuntu_debs(args):
@@ -52,19 +59,39 @@ def find_ubuntu_debs(args):
     soup.prettify()
     data = soup.get_text().split('\n')
 
+    blocks = soup.get_text().split('\n\n')
+
     ecopatt = "([45]\.x)"
     ecomatch = re.search(ecopatt, url)
+
+    ecoVersion = ''
+    try:
+        ecoVersion = '%s' % ecomatch.group(0)
+    except:
+        ecoVersion = '5.x'
 
     packagepatt = "(?<=Package: ).*"
     versionpatt = "(?<=Version: ).*"
     i = 0
-    for line in data:
-        packagematch = re.search(packagepatt, line)
-        if packagematch:
-            # print packagematch.group(0)
-            versionmatch = re.search(versionpatt, data[i + 5])
-            print "%s:%s:%s:%s" % (packagematch.group(0), versionmatch.group(0), ecomatch.group(0), "ubuntu")
-        i += 1
+    verMatch = ''
+    for block in blocks:
+        for line in block.split('\n'):
+            # print line
+            packagematch = re.search(packagepatt, line)
+            if packagematch:
+                # print packagematch.group(0)
+                for single in block.split('\n'):
+                    # print single
+                    if single.startswith('Version'):
+                        # print single
+                        versionmatch = re.search(versionpatt, single)
+                        verMatch = versionmatch.group(0)
+                        continue
+
+                # versionmatch = re.search(versionpatt, data[i + 5])
+                # print versionmatch.group(0)
+                print "%s:%s:%s:%s" % (packagematch.group(0), verMatch, ecoVersion, "ubuntu")
+            i += 1
 
 
 def main(args):
